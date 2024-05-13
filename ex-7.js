493fd3bcc74f31403d10687d4d9b2462f66d6aed
Wanchana <s.wanchana41@gmail.com>
2024-05-13 13:20:51 +0700

    fix: done ex7

--- ex-7.js
+++ ex-7.js
@@ -42,7 +42,15 @@ let orders = [
 ];
 
 // Start coding here
-let isArray;
-let creditCardTypeOfBlindermann;
-let productQuantityOfJoannet;
-let totalPurchaseOfDary;
+let isArray = console.log(Array.isArray(orders));
+let creditCardTypeOfBlindermann = orders[2].creditCardType;
+let productQuantityOfJoannet = orders[3].productQuantity;
+let totalPurchaseOfDary = orders[1].productPrice * orders[1].productQuantity;
+orders.shift();
+console.log(
+  isArray,
+  creditCardTypeOfBlindermann,
+  productQuantityOfJoannet,
+  totalPurchaseOfDary,
+  orders
+);
